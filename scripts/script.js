826a93c3e78a24736d1e45930c698f42f582cffa
Will Wu <will24m@users.noreply.github.com>
2025-08-01 15:54:39 -0400

    Remove portfolio, services, and blog pages - simplify site structure and add return to website option in admin panel

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,6 @@ document.getElementById('header').innerHTML = `
       <ul>
         <li><a href="index.html">Home</a></li>
         <li><a href="about.html">About</a></li>
-        <li><a href="portfolio.html">Portfolio</a></li>
-        <li><a href="services.html">Services</a></li>
-        <li><a href="blog.html">Blog</a></li>
         <li><a href="contact.html">Contact</a></li>
       </ul>
     </nav>
